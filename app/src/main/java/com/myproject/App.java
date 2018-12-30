0a4b3c82f1eee6bbe93dae112c059f23e03179a1
Chien-Cheng Wu <chengwu.tw@gmail.com>
2018-12-30 17:12:47 +0800

    initial version

---
+++ app/src/main/java/com/myproject/App.java
@@ -0,0 +1,20 @@
+/*
+ *  Gesture Recognizer Android Implementation
+ *  
+ *  App.java -- Saves Global App Defaults
+ *
+ * 
+ */
+
+package com.myproject;
+
+public class App {
+	// Menu items
+	public static enum MENUITEMS  {ITEM_LEARN, ITEM_RECOGNIZE, ITEM_LIBRARY};
+	// State
+	public static enum STATES {
+		STATE_LEARN, STATE_RECOGNIZE, STATE_LIBRARY;
+		
+	}
+
+}
